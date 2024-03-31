4b2c664acb6e82f3e8989cc0c187bb5ee38b1b48
Ted <54852276+Niarb144@users.noreply.github.com>
2024-03-31 21:40:04 +0300

    Add files via upload

--- js/Dicee Challenge/script.js
+++ js/Dicee Challenge/script.js
@@ -1,7 +1,12 @@
 let randomNumber1 = Math.floor((Math.random()*6));
+let randomNumber2 = Math.floor((Math.random()*6));
 let dice =['dice1', 'dice2', 'dice3', 'dice4', 'dice5', 'dice6'];
 
-let player1 = document.querySelector("img").getAttribute("src");
-document.querySelector("img").setAttribute("src", "./images/" + dice[randomNumber1] + ".png");
+let player1 = document.getElementById("player1").getAttribute("src");
+document.getElementById("player1").setAttribute("src", "./images/" + dice[randomNumber1] + ".png");
+
+let player2 = document.getElementById("player2").getAttribute("src");
+document.getElementById("player2").setAttribute("src", "./images/" + dice[randomNumber2] + ".png");
 
 console.log(randomNumber1); 
+console.log(randomNumber2);
