780adf8e37d3c6836be9a916968cd4bd02fa3cb2
Ted <54852276+Niarb144@users.noreply.github.com>
2024-03-31 22:00:29 +0300

    Add files via upload

--- js/Dicee Challenge/script.js
+++ js/Dicee Challenge/script.js
@@ -8,5 +8,18 @@ document.getElementById("player1").setAttribute("src", "./images/" + dice[random
 let player2 = document.getElementById("player2").getAttribute("src");
 document.getElementById("player2").setAttribute("src", "./images/" + dice[randomNumber2] + ".png");
 
+if(randomNumber1 > randomNumber2){
+    let message = document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    console.log("Player 1 wins");
+}
+else if( randomNumber2 > randomNumber1){
+    let message = document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    console.log("Player 2 wins");
+}
+else if( randomNumber1 === randomNumber2){
+    let message = document.querySelector("h1").innerHTML = "Draw";
+    console.log("Draw");
+}
+
 console.log(randomNumber1); 
 console.log(randomNumber2);
