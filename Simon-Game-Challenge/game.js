9624142ca3d472b6263f157727e1ab2264826af7
Ted <54852276+Niarb144@users.noreply.github.com>
2024-04-08 21:14:39 +0300

    Add files via upload

--- Simon-Game-Challenge/game.js
+++ Simon-Game-Challenge/game.js
@@ -7,10 +7,66 @@ function nextSequence(){
     let randomChosenColor = buttonColors[randomNumber];
 
     gamePattern.push(randomChosenColor);
+
+    // let yellowButton = $("#yellow");
+    // if(randomChosenColor === yellowButton){
+    //     console.log(yellowButton);
+    // }
        
     return  randomChosenColor;
 }
 
+function buttonAnimation(button){
+    $("#"+button).animate({
+        opacity: 0.5
+    });
+}
+
+$(".btn").on("click", function(event){
+    var index = $(".btn").index(this);
+    var buttonType = $(".btn")[index].id;
+    console.log(buttonType + " button clicked whose index number is " + index);
+    let key = this.buttonType;
+   
+
+    makeSound(key);
+    buttonAnimation(buttonType);
+
+    
+});
+
+// sound
+function makeSound(key){
+    switch(key){
+        case "green" :
+            var green = new Audio('./sounds/green.mp3');
+            green.play();
+            break;
+        case "red" :
+            var red = new Audio('./sounds/red.mp3');
+            red.play(); 
+            break;
+
+        case "yellow" :
+            var yellow = new Audio('./sounds/yellow.mp3');
+            yellow.play(); 
+            break;
+
+        case "blue" :
+            var blue = new Audio('./sounds/blue.mp3');
+            blue.play();
+            break;
+
+        default : 
+            var wrong = new Audio('./sounds/wrong.mp3');
+            wrong.play();
+            break;
+    }
+}
+
+
 
 console.log(nextSequence());
 console.log(gamePattern);
+
+
