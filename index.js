4c9231feb431d2d4f02674c54f3dc8bea65491a2
Ted <54852276+Niarb144@users.noreply.github.com>
2024-04-19 12:43:28 +0300

    Add files via upload
    A QR Code generator made using node. 
    Install the required packages via the terminal by typing npm i inquirer qr-image

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer  from 'inquirer';
+import fs from 'fs';
+import qr from 'qr-image';
+
+inquirer
+    .prompt([
+        {
+            message:'Enter URL',
+            name : 'URL'
+        }
+    ])
+    .then((answers) => {
+        const url = answers.URL;
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('qr.png'));
+
+        fs.writeFile('userUrl.txt', url, (err)=>{
+            if(err) throw err;
+            console.log("The file has been saved.");
+        })
+    })
+    .catch((error) => {
+    if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+    } else {
+        // Something else went wrong
+    }
+    });
+
