3af4781f187807d223d230b23d91609054c26c6f
Brian Teddy <teddybrian543@gmail.com>
2025-03-18 10:39:03 +0300

    Add usestate for increase and decrease

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  function increase(){
+    // count++
+    setCount(count + 1);
+  }
+
+  function decrease(){
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick = {decrease}>-</button>
+      <button onClick = {increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
